eafea981fec2c81670300a73e45e8ba8aa184d87
newric00 <richard.newton@unibas.ch>
2024-12-10 19:14:14 +0000

    tweaked logic to tell end of message (LF without preceding whitespace) and no longer aggregate stay-awake sends

--- GCS-parser.cpp
+++ GCS-parser.cpp
@@ -16,103 +16,110 @@ struct GCSCommandSurvey {
     std::vector<std::string> argumentNames;
 };
 
-//single-byte commands
+//single-byte queries
 const std::unordered_map<uint8_t, GCSCommandSurvey> singleByteCommands = {
+    {0x18, {"Stop All Axes", {}}},
+};
+
+//single-byte queries
+const std::unordered_map<uint8_t, GCSCommandSurvey> singleByteQueries = {
     {0x04, {"Request Status Register", {}}},
     {0x05, {"Request Motion Status", {}}},
     {0x07, {"Request Controller Ready Status", {}}},
-    {0x08, {"Query if Macro is Running", {}}},
-    {0x18, {"Stop All Axes", {}}},
+    {0x08, {"Query if Macro is Running", {}}},  
 };
 
 //multi-character commands
 const std::unordered_map<std::string, GCSCommandSurvey> multiCharCommands = {
-    {"*IDN?", {"Get Device Identification", {}}},
     {"ACC", {"Set Acceleration", {"AxisID", "Acceleration"}}},
-    {"ACC?", {"Get Acceleration", {"AxisID"}}},
     {"ADD", {"Add and Save to variable", {"Variable", "float1", "float2"}}},
     {"BRA", {"Set Brake Activation State", {"AxisID", "BrakeState"}}}, //0 = brake off, 1 = brake on
-    {"BRA?", {"Get Brake Activation State", {"AxisID"}}},
     {"CPY", {"Copy into Variable", {"Variable", "CMD?"}}},
-    {"CST?", {"Get Assignment of Stages to Axes", {"AxisID"}}},
-    {"CSV?", {"Get Current Syntax Version", {}}},
     {"CTO", {"Set Configuration of Trigger Output", {"TrigOutID", "CTOParam", "Value"}}},
-    {"CTO?", {"Get Configuration of Trigger Output", {"TrigOutID", "CTOParam"}}},
     {"DEC", {"Set Deceleration", {"AxisID", "Deceleration"}}},
-    {"DEC?", {"Get Deceleration", {"AxisID"}}},
     {"DEL", {"Delay the Command Interpreter", {"Delay"}}}, //ms
     {"DIO", {"Set Digital Output Line", {"DIOID", "Output"}}}, //1 = high, 0 = Low
-    {"DIO?", {"Get Digital Output Line", {"DIOID"}}},
     {"DRC", {"Set Data Recorder Configuration", {"RecTableID", "Source", "RecOption"}}},
-    {"DRC?", {"Get Data Recorder Configuration", {"RecTableID"}}},
-    {"DRR?", {"Get Recorded Data Values", {"StartPoint", "NumberOfPoints","RecTableID"}}},
     {"DRT", {"Set Data Recorder Trigger Source", {"RecTableID", "TriggerSource", "Value"}}},
-    {"DRT?", {"Get Data Recorder Trigger Source", {"RecTableID"}}},
-    {"ERR?", {"Get Error Number", {}}},
     {"FED", {"Find Edge", {"AxisID", "EdgeID", "Param"}}},
     {"FNL", {"Fast Reference Move to Negative Limit", {"AxisID"}}},
     {"FPL", {"Fast Reference Move to Positive Limit", {"AxisID"}}},
     {"FRF", {"Fast Reference Move to Reference Switch", {"AxisID"}}},
-    {"FRF?", {"Get Referencing Result", {"AxisID"}}},
     {"GOH", {"Go to Home Position", {"AxisID"}}},
-    {"HDR?", {"Get All Data Recorder Options", {}}},
     {"HLP", {"Get List of Available Commands", {}}},
     {"HLT", {"Halt Motion Smoothly", {"AxisID"}}},
-    {"HPA?", {"Get List of Available Parameters", {}}},
-    {"JAS?", {"Query Joystick Axis Status", {"JoystickID", "JoyStickAxis"}}},
     {"JAX", {"Set Axis Controlled by Joystick", {"JoystickID", "JoyStickAxis", "AxisID"}}},
-    {"JAX?", {"Get Axis Controlled by Joystick", {"JoystickID", "JoyStickAxis"}}},
-    {"JAX?", {"Get Axis Controlled by Joystick", {"JoystickID", "JoyStickAxis"}}},
-    {"JBS?", {"Query Joystick Button Status", {"JoystickID", "JoyStickButton"}}},
     {"JDT", {"Set Joystick Default Lookup Table", {"JoystickID", "JoyStickAxis", "LookupTableProfileType"}}}, //1 = linear (default), 2 = parabolic
     {"JLT", {"Fill Joystick Lookup Table", {"JoystickID", "JoyStickAxis", "Address", "Trajectory"}}},
-    {"JLT?", {"Get Joystick Lookup Table Values", {"StartAddress", "NumberOfAddressReads", "JoystickID", "JoyStickAxis"}}},
     {"JON", {"Set Joystick Activation Status", {"JoystickID", "ActivationStatus"}}}, //0 = disable, 1 = enable
     {"JON?", {"Get Joystick Activation Status", {"JoystickID"}}},
     {"JRC", {"Jump Relatively Depending on Condition", {"JumpSize", "CMD?", "Operator", "Value"}}},
-    {"LIM?", {"Indicate Limit Switches", {"AxisID"}}},
     {"MAC", {"Call Macro Function", {"Keyword", "Parameters"}}}, //Keywords = BEG, DEF, DEF?, DEL, END, ERR?, NSTART, START
-    {"MAC?", {"List Macros", {"MacroName"}}},
     {"MEX", {"Stop Macro Execution due to Condition", {"CMD?", "Operator", "Value"}}},
     {"MOV", {"Set Target Position", {"AxisID", "Position"}}},
-    {"MOV?", {"Get Target Position", {"AxisID"}}},
     {"MVR", {"Set Target Relative to Current Position", {"AxisID", "Distance"}}},
-    {"ONT?", {"Get On Target State", {"AxisID"}}},
     {"POS", {"Set Real Position", {"AxisID", "Position"}}}, //without motion
     {"POS?", {"Get Real Position", {"AxisID"}}},
     {"RBT", {"Reboot System", {}}},
-    {"RMC?", {"List Running Macros", {}}},
     {"RON", {"Set Reference Mode", {"AxisID", "ReferenceOut"}}},
     {"RON?", {"Get Reference Mode", {"AxisID"}}},
     {"RPA", {"Reset Volatile Memory Parameters", {"ItemID", "ParamID"}}},
     {"RTR", {"Set Record Table Rate", {"RecordTableRate"}}},
     {"RTR?", {"Get Record Table Rate", {}}},
     {"SAI", {"Set Current Axis Identifiers", {"AxisID", "NewIdentifier"}}},
-    {"SAI?", {"Get Current Axis Identifiers", {"ALL"}}}, //If ALL sent as arguemnet deactivated axes also respond
     {"SEP", {"Set Non-Volatile Memory Parameters", {"Password", "ItemID", "ParamID", "ParamValue"}}}, // Default password = 100
     {"SPA", {"Set Volatile Memory Parameters", {"ItemID", "ParamID", "ParamValue"}}},
-    {"SPA?", {"Get Volatile Memory Parameters", {"ItemID", "ParamID"}}},
-    {"SRG?", {"Query Status Register Value", {"AxisID", "RegisterID"}}},
     {"STE", {"Start Step and Response Measurement", {"AxisID", "Amplitude"}}},
     {"STP", {"Stop All Axes", {}}},
     {"SVO", {"Set Motor State", {"AxisID", "MotorState"}}}, //0 = servo off, 1 = servo on
+    {"TAV", {"Get Analog Input Voltage", {"AnalogInputID"}}},
+    {"TRO", {"Set Trigger Output State", {"TrigOutID", "TrigMode"}}}, //0 = trigger output disabled, 1 = trigger output enabled
+    {"VAR", {"Set Variable Value", {"Variable", "String"}}},
+    {"VEL", {"Set Velocity", {"AxisID", "Velocity"}}},
+    {"VER", {"Get Versions of Firmware and Drivers", {}}},
+    {"WAC", {"Wait for Condition", {"CMD?", "Operator", "Value"}}},
+};
+
+//multi-character queries
+const std::unordered_map<std::string, GCSCommandSurvey> multiCharQueries = {
+    {"*IDN?", {"Get Device Identification", {}}},
+    {"ACC?", {"Get Acceleration", {"AxisID"}}},
+    {"BRA?", {"Get Brake Activation State", {"AxisID"}}},
+    {"CST?", {"Get Assignment of Stages to Axes", {"AxisID"}}},
+    {"CSV?", {"Get Current Syntax Version", {}}},
+    {"CTO?", {"Get Configuration of Trigger Output", {"TrigOutID", "CTOParam"}}},
+    {"DEC?", {"Get Deceleration", {"AxisID"}}},
+    {"DIO?", {"Get Digital Output Line", {"DIOID"}}},
+    {"DRC?", {"Get Data Recorder Configuration", {"RecTableID"}}},
+    {"DRR?", {"Get Recorded Data Values", {"StartPoint", "NumberOfPoints","RecTableID"}}},
+    {"DRT?", {"Get Data Recorder Trigger Source", {"RecTableID"}}},
+    {"ERR?", {"Get Error Number", {}}},
+    {"FRF?", {"Get Referencing Result", {"AxisID"}}},
+    {"HDR?", {"Get All Data Recorder Options", {}}},
+    {"HPA?", {"Get List of Available Parameters", {}}},
+    {"JAS?", {"Query Joystick Axis Status", {"JoystickID", "JoyStickAxis"}}},
+    {"JAX?", {"Get Axis Controlled by Joystick", {"JoystickID", "JoyStickAxis"}}},
+    {"JBS?", {"Query Joystick Button Status", {"JoystickID", "JoyStickButton"}}},
+    {"JLT?", {"Get Joystick Lookup Table Values", {"StartAddress", "NumberOfAddressReads", "JoystickID", "JoyStickAxis"}}},
+    {"LIM?", {"Indicate Limit Switches", {"AxisID"}}},
+    {"MAC?", {"List Macros", {"MacroName"}}},
+    {"MOV?", {"Get Target Position", {"AxisID"}}},
+    {"ONT?", {"Get On Target State", {"AxisID"}}},
+    {"RMC?", {"List Running Macros", {}}},
+    {"SAI?", {"Get Current Axis Identifiers", {"ALL"}}}, //If ALL sent as arguemnet deactivated axes also respond
+    {"SPA?", {"Get Volatile Memory Parameters", {"ItemID", "ParamID"}}},
+    {"SRG?", {"Query Status Register Value", {"AxisID", "RegisterID"}}},
     {"SVO?", {"Get Motor State", {"AxisID"}}},
     {"TAC?", {"Tell Analog Channels", {}}},
-    {"TAV", {"Get Analog Input Voltage", {"AnalogInputID"}}},
     {"TIO?", {"Tell Digital I/O Lines", {}}},
     {"TMN?", {"Get Minimum Commandable Position", {"AxisID"}}},
     {"TMX?", {"Get Maximum Commandable Position", {"AxisID"}}},
     {"TNR?", {"Get Number of Record Tables", {}}},
-    {"TRO", {"Set Trigger Output State", {"TrigOutID", "TrigMode"}}}, //0 = trigger output disabled, 1 = trigger output enabled
     {"TRO?", {"Get Trigger Output State", {"TrigOutID"}}},
     {"TRS?", {"Indicate Reference Switch", {"AxisID"}}},
     {"TVI?", {"Tell Valid Character Set for Axis Identifiers", {}}},
-    {"VAR", {"Set Variable Value", {"Variable", "String"}}},
     {"VAR?", {"Get Variable Value", {"Variable"}}},
-    {"VEL", {"Set Velocity", {"AxisID", "Velocity"}}},
     {"VEL?", {"Get Velocity", {"AxisID"}}},
-    {"VER", {"Get Versions of Firmware and Drivers", {}}},
-    {"WAC", {"Wait for Condition", {"CMD?", "Operator", "Value"}}},
 };
 
 std::optional<GCSCommandSurvey> getCommandInfo(const std::string& command) {
@@ -121,12 +128,18 @@ std::optional<GCSCommandSurvey> getCommandInfo(const std::string& command) {
         if (singleByteCommands.find(singleByte) != singleByteCommands.end()) {
             return singleByteCommands.at(singleByte);
         }
+        if (singleByteCommands.find(singleByte) != singleByteQueries.end()) {
+            return singleByteCommands.at(singleByte);
+        }
+    }
+    if (command.find('?') != std::string::npos) {
+        if (multiCharQueries.find(command) != multiCharQueries.end()) {
+            return multiCharQueries.at(command);
+        }
     }
     if (multiCharCommands.find(command) != multiCharCommands.end()) {
-        //std::cout << "Found multi-character command: " << command << std::endl; // debugging
         return multiCharCommands.at(command);
     }
- 
     //command not found
     //std::cout << "Command not found: " << command << std::endl; // debugging
     return std::nullopt;
@@ -183,18 +196,29 @@ std::string cleanResponse(const std::string& response) {
     return cleanedResponse;
 }
 std::optional<std::string> handleBulkInResponse(const std::vector<uint8_t>& data) {
-    std::string fragment(data.begin(), data.end());
-    responseBuffer += fragment;
+    std::string responseFragment(data.begin(), data.end());
+    static std::string aggregatedResponse;
+    
+    //if simple stay-awake command.  Do not aggregate.
+    responseFragment.erase(std::remove_if(responseFragment.begin(), responseFragment.end(),
+        [](unsigned char c) { return c < 17 && c != '\n'; }), responseFragment.end());
 
-    if (!responseBuffer.empty() && responseBuffer.back() == '\n') {
-        std::string aggregatedResponse = responseBuffer;
-        responseBuffer.clear();
-        std::string cleanedResponse = cleanResponse(aggregatedResponse);
-        std::vector<uint8_t> aggregatedData(cleanedResponse.begin(), cleanedResponse.end());
-        std::string parsedResponse = parseGCSCommand(aggregatedData);
-        return parsedResponse;
+    if (responseFragment == "`") {
+        return std::nullopt;
     }
-    //incomplete response (no terminating LF)
-    //bulkInByteCount += data.size();
+
+    aggregatedResponse += responseFragment;
+    //multi-line response are terminated with a LF character.  Intermediate lines with a white space followed by LF
+    if (!aggregatedResponse.empty() && aggregatedResponse.back() == '\n') {
+        if (aggregatedResponse.size() > 1 && aggregatedResponse[aggregatedResponse.size() - 2] != ' ') {
+            std::string cleanedResponse = cleanResponse(aggregatedResponse);
+            std::vector<uint8_t> aggregatedData(cleanedResponse.begin(), cleanedResponse.end());
+            std::string parsedResponse = parseGCSCommand(aggregatedData);
+            aggregatedResponse.clear();
+            return parsedResponse;
+        }
+        //incomplete response (no terminating LF without preceding whitespace)
+    }
+    //continue aggregating
     return std::nullopt;
 }
